c39a65eedada69385ac0f6f52eec447300d53a1f
Thomas Hart <tdh2124@columbia.edu>
2023-02-25 21:56:51 +0000

    updated streamlit app to contain table

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+file_path='https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+df=pandas.read_csv(file_path)
+
+streamlit.dataframe(df)
