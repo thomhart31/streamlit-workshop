b0c28fcd93295015ea69d1c4396000d63eb6388a
Thomas Hart <tdh2124@columbia.edu>
2023-02-25 21:59:51 +0000

    added a multiselect to app

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 file_path='https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
-df=pandas.read_csv(file_path)
 
-streamlit.dataframe(df)
+dataframe=pandas.read_csv(file_path).set_index('Fruit')
+
+streamlit.multiselect('Pick some fruits:', list(dataframe.index))
+
+streamlit.dataframe(dataframe)
+
+# adding in a multi select
