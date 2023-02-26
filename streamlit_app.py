facde4fdf9801113337becd054737c1271f3c9a7
Thomas Hart <tdh2124@columbia.edu>
2023-02-26 15:06:37 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.title('Streamlit Workshop Example')
 
@@ -30,3 +31,12 @@ streamlit.text(response.json())
 
 dataframe_fruityvice = pandas.json_normalize(response.json())
 streamlit.dataframe(dataframe_fruityvice)
+
+# connecting to snowflake
+streamlit.secrets['snowflake']
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
