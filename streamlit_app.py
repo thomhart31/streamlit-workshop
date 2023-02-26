7967eec6e3d69bf657f0d6a765cc0a9d3cbec706
Thomas Hart <tdh2124@columbia.edu>
2023-02-26 15:09:19 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,14 @@ dataframe_fruityvice = pandas.json_normalize(response.json())
 streamlit.dataframe(dataframe_fruityvice)
 
 # connecting to snowflake
-# streamlit.secrets['snowflake']
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
+
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
