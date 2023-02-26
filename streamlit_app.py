465bd969300c16022380f61994a04707b3d0ddf6
Thomas Hart <tdh2124@columbia.edu>
2023-02-26 15:12:01 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+choice2 = streamlit.text_input(label='What would you like to add:')
+streamlit.text(f'Thanks for adding: {choice2}')
+
